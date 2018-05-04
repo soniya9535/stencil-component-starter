c1e8b69c5d12644cf198fc18625686d349a12d21
Soniya Praba <spraba@sapient.com>
2018-05-04 18:43:38 +0530

    stencil component life cycle

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -10,11 +10,58 @@ export class MyComponent {
   @Prop() first: string;
   @Prop() last: string;
 
+ /**
+   * The component will load but has not rendered yet.
+   *
+   * This is a good place to make any last minute updates before rendering.
+   *
+   * Will only be called once
+   */
+  componentWillLoad() {
+    console.log('The component is about to be rendered');
+  }
+
+  /**
+   * The component is loaded and has rendered.
+   *
+   * Updating data in this event may cause the component to re-render.
+   *
+   * Will only be called once
+   */
+  componentDidLoad() {
+    console.log('The component has been rendered');
+  }
+
+  /**
+   * The component will update and re-render.
+   *
+   * Called multiple times throughout the life of the component as it updates.
+   */
+  componentWillUpdate() {
+    console.log('The component will update');
+  }
+
+  /**
+   * The component finished updating.
+   *
+   * Called after componentWillUpdate
+   *
+   * Called multiple times throughout the life of the component as it updates.
+   */
+  componentDidUpdate() {
+    console.log('The component did update');
+  }
+
+  /**
+   * The component did unload and the element will be destroyed.
+   */
+  componentDidUnload() {
+    console.log('The view has been removed from the DOM');
+  }
+
   render() {
-    return (
-      <div>
-        Hello, World! I'm {this.first} {this.last}
-      </div>
-    );
+    return(
+      <p>Stencil component life cycle</p>
+    )
   }
 }
